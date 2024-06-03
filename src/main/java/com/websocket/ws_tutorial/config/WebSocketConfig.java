84d311c3e0cd60b802b08f026b699d09cfe753fa
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-03 21:35:26 +0300

    * Restructure WebSocketConfig.

--- src/main/java/com/websocket/ws_tutorial/config/WebSocketConfig.java
+++ src/main/java/com/websocket/ws_tutorial/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.websocket.ws_tutorial;
+package com.websocket.ws_tutorial.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
