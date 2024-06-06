b3b9eae93cb5ed24894f3f1f23337a8c718f2fe6
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-06 08:40:05 +0300

    * Add Comments WebSocketConfig.

--- src/main/java/com/websocket/ws_tutorial/config/WebSocketConfig.java
+++ src/main/java/com/websocket/ws_tutorial/config/WebSocketConfig.java
@@ -8,7 +8,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+//WebSocketMessageBrokerConfigurer: Interface for connect with server to establish connection.
+//@EnableWebSocketMessageBroker: Annotation to allow WS implement to WebSocketMessageBrokerConfigurer.
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // registerStompEndpoints to select URL to connect with server.
+    // STOMP: Streaming Text Messaging Protocol.
+    // STOMP: exchange and transfer data.
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/our-websocket").withSockJS();
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -16,8 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/ws");
     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/our-websocket").withSockJS();
-    }
+    /* Note: WebSocket just open connection between client side and server side
+             WebSocket can not define roles or users or transfer data
+     */
 }
