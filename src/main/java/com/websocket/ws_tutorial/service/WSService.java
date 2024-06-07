9910ccef70f1db711e8644bafdbbac0c26cc96ff
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-07 10:35:59 +0300

    * Test Send Message.

---
+++ src/main/java/com/websocket/ws_tutorial/service/WSService.java
@@ -0,0 +1,24 @@
+package com.websocket.ws_tutorial.service;
+
+import com.websocket.ws_tutorial.dto.ResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WSService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+
+    public WSService(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public void notifyFrontend(final String message) {
+        ResponseMessage response = new ResponseMessage(message);
+
+        messagingTemplate.convertAndSend("/topic/messages", response);
+    }
+}
