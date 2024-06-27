eeccc4f9629ac123d3301af50a6e24144b903f35
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-27 11:17:31 +0300

    Websocket push notification

--- src/main/java/com/websocket/ws_tutorial/controller/MessageController.java
+++ src/main/java/com/websocket/ws_tutorial/controller/MessageController.java
@@ -2,6 +2,8 @@ package com.websocket.ws_tutorial.controller;
 
 import com.websocket.ws_tutorial.dto.Message;
 import com.websocket.ws_tutorial.dto.ResponseMessage;
+import com.websocket.ws_tutorial.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.annotation.SendToUser;
@@ -13,12 +15,15 @@ import java.security.Principal;
 @Controller
 public class MessageController {
 
+    @Autowired
+    private NotificationService notificationService;
+
     @MessageMapping("/message")
     @SendTo("/topic/messages")
     public ResponseMessage getMessage(final Message message) throws InterruptedException {
 
         Thread.sleep(1000);
-
+        notificationService.sendGlobalNotification();
         return new ResponseMessage(HtmlUtils.htmlEscape(message.getMessageContent()));
     }
 
@@ -28,6 +33,7 @@ public class MessageController {
                                              final Principal principal)
         throws InterruptedException {
         Thread.sleep(1000);
+        notificationService.sendPrivateNotification(principal.getName());
         return new ResponseMessage(HtmlUtils.htmlEscape(
                 "Sending private message to user " + principal.getName() +
                         ": " + message.getMessageContent()
--- src/main/java/com/websocket/ws_tutorial/service/WSService.java
+++ src/main/java/com/websocket/ws_tutorial/service/WSService.java
@@ -9,22 +9,24 @@ import org.springframework.stereotype.Service;
 public class WSService {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final NotificationService notificationService;
 
     @Autowired
-
-    public WSService(SimpMessagingTemplate messagingTemplate) {
+    public WSService(SimpMessagingTemplate messagingTemplate,
+                     NotificationService notificationService) {
         this.messagingTemplate = messagingTemplate;
+        this.notificationService = notificationService;
     }
 
     public void notifyFrontend(final String message) {
         ResponseMessage response = new ResponseMessage(message);
-
+        notificationService.sendGlobalNotification();
         messagingTemplate.convertAndSend("/topic/messages", response);  // Send Message To /topic/messages
     }
 
     public void notifyUser(final String id, final String message) {
         ResponseMessage response = new ResponseMessage(message);
-
+        notificationService.sendPrivateNotification(id);
         messagingTemplate.convertAndSendToUser(id, "/topic/private-messages", response);
     }
 }
